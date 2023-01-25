719e836237e8106578b4bae4f9b730902666e2c1
suvidhaks <32566719+suvidhaks@users.noreply.github.com>
2023-01-26 01:36:33 +0530

    Add files via upload

--- bitmask_ANDOR.c
+++ bitmask_ANDOR.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-#define SETBIT 0177
+#define SETBIT 0177700
 int main()
 {
-  int a=0,c,d;
+  int a=0,c,d,e,f,unary,u=5;
   c=a&SETBIT;
   d=a|SETBIT;
-  printf("c=%d\td=%d", c,d);
+  e=8>>1;
+   f=8<<1;
+   unary=~u;
+  printf("%d\t%d", c,d);
+  printf("\n%d\t %d", e, f);
+  printf("\n2'scompliment unsigned no.=%d", unary);
   return 0;
 }
