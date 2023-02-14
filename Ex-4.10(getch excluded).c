7b025b2cd647e226f1c4c8a1f91d12ebe9f0d8e6
suvidhaks <32566719+suvidhaks@users.noreply.github.com>
2023-02-14 17:45:44 +0530

    Add files via upload

---
+++ Ex-4.10(getch excluded).c
@@ -0,0 +1,190 @@
+/*
+this program works for integer values
+input: 1 2 - 4 5 + *
+o/p: -9
+*/
+
+
+#include <stdio.h>
+#include<ctype.h>
+#define MAX 100
+#define NUMBER '0'
+int a[MAX];
+//char s[MAX];
+char buff[MAX];
+int bufp=0;
+int dp=0;
+//int i=0;
+int getop(char s[]);
+int asint(char s[]);
+void push(int);
+int pop(void);
+//int getch();
+
+int main()
+{
+    //double d;
+    int d;
+    int type;
+    char s[MAX];
+    int op2;
+     int val1;
+     int a,b, i1;
+     int flag1;
+while((type=getop(s))!='\n')
+{
+     switch(type)
+    {
+        
+        case NUMBER: 
+                     
+                      val1=asint(s);
+                     push(asint(s));  // atofl() is passed as an argument since the return type of atofl is double; and push accepts the arg double
+                     
+                      break;
+
+         case '+': push(pop() + pop());  //pop returns double;
+                   // printf("\nlf=%lf", a[dp]);    
+                    break;
+
+         case '*' :  push(pop() * pop());  
+                   // printf("\nlf=%lf", a[--dp]); 
+                     break;
+
+           case '-' : op2=pop();
+                       op2=op2*(-1);
+                       push(op2);
+                        // printf("\nlf=%lf", a[dp]);
+                       break;
+
+            case '/' : op2=pop();
+                        if(op2!=0)
+                        {
+                           push(pop() / op2);
+                        }
+                         // printf("\nlf=%lf", a[dp]);
+                       break; 
+
+            case '%' : b=pop(); 
+                       a=pop();    
+                       if(a>=0 && b!=0) 
+                       {
+                         if(b>a)
+                         {
+                            push(a);
+                         }           
+                        else
+                        {
+                           push(a%b);
+                        }
+                       }
+                      
+                      else if(a<0 && b!=0)
+                      {
+                        for(i1=0;i1<b;i1++)
+                        {
+                               if((-(a-i1))%b==0)
+                              {
+                                     flag1=i1;
+                                      break;
+                                  }
+                         }
+                        push(flag1);
+                      }
+
+                        break;
+                        
+           /*  case '\n':      printf("\nresult=%d", pop());  
+                              // printf("\ndp=%d", dp);                     
+                              break; */
+                              
+    }  
+}                
+
+    printf("\nresult=%d", pop());  
+    return 0;
+}
+
+
+int asint(char s[])  // in this function only base address of s is passed everytime the function is called;
+{
+    
+    int  sign;
+    int i=0;
+    int val;
+    
+    for(;isspace(s[i]);i++);
+    sign=(s[i]=='-')?-1:1;
+    if(s[i]=='-')
+       i++;
+    for(val=0;isdigit(s[i]);i++)
+    {
+        val=val*10+s[i]-'0';
+    }
+   
+    return sign*(val);
+}
+
+
+int getop(char s[])
+{
+    int i;
+    int c;
+    
+    while((s[0]=c=getchar())==' ' || c=='\t');  //  the significance of this while loop is if you encounter space it; the function returns the value
+    s[1]='\0';
+    if(!isdigit(c))
+    {
+        return c;
+    }
+    i=0;
+    if(isdigit(s[i]))
+    {
+        i++;
+    }
+    for(;isdigit(s[i]=c=getchar());i++);
+    s[i]='\0';
+   
+    return NUMBER;
+}
+
+
+
+
+
+
+void push(int f)
+{
+    if(dp<MAX)
+    {
+         a[dp++]=f;
+        
+    }
+   else
+   {
+       printf("stack is full");
+   }
+    
+}
+
+
+int pop(void)
+{
+    if(dp>0)
+    {
+        return a[--dp];
+    }
+    else
+    {
+     printf("stack is empty");
+     return 0.0;
+    }
+}
+
+
+
+
+
+
+
+
