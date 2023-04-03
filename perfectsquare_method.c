d1b806c8dfed13157f1cdafd3d6e465aec3d3b6b
suvidhaks <32566719+suvidhaks@users.noreply.github.com>
2023-04-03 11:52:39 +0530

    Add files via upload

--- perfectsquare_method.c
+++ perfectsquare_method.c
@@ -1,4 +1,13 @@
+/*
+this program checks if a given number is perfect square or not;
+if it is perfect square; the square root of that perfect square will be displayed;
+function to find squareroot of a perfect square usingprime factorization has been implemented;
+
+*/
+
+
 #include<stdio.h>
+#include<math.h>
 void perfectsquare(int n);
 int main()
 {
@@ -13,25 +22,40 @@ int main()
 void perfectsquare(int n)
 {
    int exp=0,  i, flag=1;
-   int value=n;
-
+   int value=n, fact=1, j=0;
+   int prod=1;
    for(i=2;i<value;i++)
    {
+      fact=1, j=0;
     while(n%i==0)
     {
         n=n/i;
         exp++;
       //  printf("%d\t%d ", exp,n);
+      fact=i;
+      j++;
+      fact=pow(fact, j/2); // finding square root of a perfect square;
+      
     }
+    
     if(exp%2!=0 )
     {
         flag=0;
         printf("\n not a aperfect square");
          break;
     }
+    else if(exp%2==0)
+    {
+      prod*=fact;
+    }
    }
   // printf("i=%d", i);
    
+   //printf("\nsquareroot=%d ", fact);
+
+   // fact=pow(fact, j/2);
+    // printf("\n j=%d ", j);
+     printf("\nsquareroot=%d ", prod);
 
    if(flag && n!=value)
   {
